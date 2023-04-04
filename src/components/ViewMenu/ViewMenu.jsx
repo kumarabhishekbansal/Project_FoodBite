d5e92f93de62bd36e35dcf5b20038b07777a7639
kumarabhishekbansal <kumarabhishekbansal>
2023-04-04 12:56:47 +0530

    home page 1

---
+++ src/components/ViewMenu/ViewMenu.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Rescards from '../rescards/Rescards'
+import './style.css'
+const ViewMenu = () => {
+  return (
+    <>
+
+        <div className="section1">
+          
+        </div>
+
+
+
+            {/* cards */}
+
+      <div className="carddiv">
+        <Rescards />
+        <Rescards />
+        <Rescards />
+        <Rescards />
+        <Rescards />
+        <Rescards />
+        <Rescards />
+        <Rescards />
+      </div>
+    </>
+  )
+}
+
+export default ViewMenu
