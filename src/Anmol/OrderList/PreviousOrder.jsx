1d03c3d35ded9dc0bd5b3967558242b15e6ae32f
kumarabhishekbansal <kumarabhishekbansal>
2023-05-14 18:44:05 +0530

    Pre Complete

--- src/Anmol/OrderList/PreviousOrder.jsx
+++ src/Anmol/OrderList/PreviousOrder.jsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import "./PreviousOrder.css";
 import Moment from "react-moment";
 import "../Images/bg2.jpg";
+import axios from "axios";
 import Shop from "../components/Empty/Shop";
 import EmptyOrder from "../components/Empty/EmptyOrder";
 import { resorderreset } from "../../Reducers/ResOrder/ResOrderSlice";
@@ -16,6 +17,15 @@ import DeliveryDiningIcon from "@mui/icons-material/DeliveryDining";
 import TaskAltIcon from "@mui/icons-material/TaskAlt";
 import BeenhereIcon from "@mui/icons-material/Beenhere";
 
+// feedback emojis
+
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import SentimentDissatisfiedIcon from '@mui/icons-material/SentimentDissatisfied';
+import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
+import MoodIcon from '@mui/icons-material/Mood';
+
+let API_URL='/api/res/'
 const PreviousOrder = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -31,6 +41,102 @@ const PreviousOrder = () => {
   const [orders, setorders] = useState([]);
 
   const [orderflag, setorderflag] = useState(false);
+
+    // feedback flags
+
+    const [iconflag1,seticonflag1]=useState(false);
+    const [iconflag2,seticonflag2]=useState(false);
+    const [iconflag3,seticonflag3]=useState(false);
+    const [iconflag4,seticonflag4]=useState(false);
+    const [iconflag5,seticonflag5]=useState(false);
+    const[star,setstar]=useState(0);
+
+
+  // feeback functions
+
+  const feedback1=async(resId)=>{
+      seticonflag1(true);
+      seticonflag2(false);
+      seticonflag3(false);
+      seticonflag4(false);
+      seticonflag5(false);
+      setstar(1);
+      const data={
+        star:1,
+        resId:resId._id
+      }
+      // console.log(data);
+      const response=await axios.post(API_URL+`addstars/${user._id}`,data);
+      // console.log(response);
+  }
+
+  const feedback2=async(resId)=>{
+    seticonflag1(false);
+    seticonflag2(true);
+    seticonflag3(false);
+    seticonflag4(false);
+    seticonflag5(false);
+    setstar(2);
+    const data={
+      star:2,
+      resId:resId._id
+    }
+    // console.log(data);
+    const response=await axios.post(API_URL+`addstars/${user._id}`,data);
+    // console.log(response);
+  }
+
+  const feedback3=async(resId)=>{
+    seticonflag1(false);
+    seticonflag2(false);
+    seticonflag3(true);
+    seticonflag4(false);
+    seticonflag5(false);
+    setstar(3);
+    const data={
+      star:3,
+      resId:resId._id
+    }
+    // console.log(data);
+    const response=await axios.post(API_URL+`addstars/${user._id}`,data);
+    // console.log(response);
+  }
+
+  const feedback4=async(resId)=>{
+    seticonflag1(false);
+    seticonflag2(false);
+    seticonflag3(false);
+    seticonflag4(true);
+    seticonflag5(false);
+    setstar(4);
+    const data={
+      star:4,
+      resId:resId._id
+    }
+    // console.log(data);
+    const response=await axios.post(API_URL+`addstars/${user._id}`,data);
+    // console.log(response);
+  }
+
+  const feedback5=async(resId)=>{
+    seticonflag1(false);
+    seticonflag2(false);
+    seticonflag3(false);
+    seticonflag4(false);
+    seticonflag5(true);
+    setstar(5);
+    const data={
+      star:5,
+      resId:resId._id
+    }
+    // console.log(data);
+    const response=await axios.post(API_URL+`addstars/${user._id}`,data);
+    // console.log(response);
+  }
+
+
+
+
 
   useEffect(() => {
     dispatch(getuser(user._id));
@@ -246,6 +352,56 @@ const PreviousOrder = () => {
                             </div>
                           </div>
                           <br />
+
+
+                          {/* taking feedback */}
+
+                          {val.orderstatus==="Completed"?(<>
+                            <div className="feedback_main_div">
+                            <h1>FEEDBACK:</h1>
+                            <div className="SentimentVeryDissatisfiedIcon_div feedback_icon">
+                              {iconflag1 || val.resId.totalRating==='1'?(<>
+                                <SentimentVeryDissatisfiedIcon style={{fontSize:"5rem",color:'blue'}}/>
+                              </>):(<>
+                                <SentimentVeryDissatisfiedIcon style={{fontSize:"5rem",color:'red'}} onClick={()=>feedback1(val.resId)}/>
+                              </>)}
+                              
+                            </div>
+                            <div className="SentimentDissatisfiedIcon_div feedback_icon">
+                              {iconflag2 || val.resId.totalRating==='2'?(<>
+                                <SentimentDissatisfiedIcon  style={{fontSize:"5rem",color:'blue'}}/>
+                              </>):(<>
+                                <SentimentDissatisfiedIcon  style={{fontSize:"5rem",color:'red'}} onClick={()=>feedback2(val.resId)}/>
+                              </>)}
+                              
+                            </div>
+                            <div className="SentimentSatisfiedIcon_div feedback_icon">
+                              {iconflag3 || val.resId.totalRating==='3'?(<>
+                                <SentimentSatisfiedIcon style={{fontSize:"5rem",color:'blue'}} />
+                              </>):(<>
+                                <SentimentSatisfiedIcon style={{fontSize:"5rem",color:'red'}} onClick={()=>feedback3(val.resId)}/>
+                              </>)}
+                              
+                            </div>
+                            <div className="SentimentSatisfiedAltIcon_div feedback_icon">
+                              {iconflag4 || val.resId.totalRating==='4'?(<>
+                                <SentimentSatisfiedAltIcon  style={{fontSize:"5rem",color:'blue'}}/>
+                              </>):(<>
+                                <SentimentSatisfiedAltIcon  style={{fontSize:"5rem",color:'red'}} onClick={()=>feedback4(val.resId)}/>
+                              </>)}
+                              
+                            </div>
+                            <div className="MoodIcon_div feedback_icon">
+                            {iconflag5 || val.resId.totalRating==='5'?(<>
+                              <MoodIcon style={{fontSize:"5rem",color:'blue'}}/>
+                            </>):(<>
+                              <MoodIcon style={{fontSize:"5rem",color:'red'}} onClick={()=>feedback5(val.resId)}/>
+                            </>)}
+                              
+                            </div>
+                          </div>
+                          </>):(<></>)}
+
                           <h3 className="order_detail_heading">Order Details</h3>
                           {val.orderDetails.items.map((valitem) => {
                             return (
